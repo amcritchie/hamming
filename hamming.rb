1e0808b3f7e05fe7164aed8909ac38b79800a09c
Alex McRitchie <amcritchie@gmail.com>
2014-06-18 14:47:27 -0600

    Finished solo work.

--- hamming.rb
+++ hamming.rb
@@ -2,6 +2,24 @@ require_relative "testing_library"
 
 def hamming(strand_1, strand_2)
 
+  count = 0
+  hamming_distance = 0
+
+  if strand_1 <= strand_2
+    dna1 = strand_1.split("")
+    dna2 = strand_2.split("")
+  else
+    dna1 = strand_2.split("")
+    dna2 = strand_1.split("")
+  end
+  
+  dna1.each do |letter|
+    if letter != dna2[count]
+      hamming_distance += 1
+    end
+    count += 1
+  end
+  hamming_distance
 end
 
 check("Hamming distance between identical strands",
